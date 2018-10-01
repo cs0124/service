8a036300d5bb702190afa4c612bbafd31efb5c02
cs0124 <chis666@163.com>
2018-10-01 20:10:51 +0800

    api-gateway

---
+++ icmes-api-gateway/src/main/java/com/dtelec/icmes/gateway/GatewayApplication.java
@@ -0,0 +1,75 @@
+package com.dtelec.icmes.gateway;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@EnableSwagger2Doc
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	@Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+	
+	
+	@Component
+    @Primary
+    class DocumentationConfig implements SwaggerResourcesProvider {
+		
+		@Override
+        public List<SwaggerResource> get() {
+        	List<SwaggerResource> resources = new ArrayList<>();
+
+            resources.add(swaggerResource("ICMES-授权子系统", "/uaa/v2/api-docs", "2.0"));
+            resources.add(swaggerResource("ICMES-账户子系统", "/account/v2/api-docs", "2.0"));
+            resources.add(swaggerResource("ICMES-信息子系统", "/info/v2/api-docs", "2.0"));
+
+            return resources;
+        }
+
+		
+        private SwaggerResource swaggerResource(String name, String location, String version) {
+            SwaggerResource swaggerResource = new SwaggerResource();
+            swaggerResource.setName(name);
+            swaggerResource.setLocation(location);
+            swaggerResource.setSwaggerVersion(version);
+            return swaggerResource;
+        }
+    }
+}
